b32e1044bf055dcdd696840ad80a3f649679642b
jianwei <jianwei@outlook.com>
2016-07-09 23:23:46 +0800

    add Properties demonstration

---
+++ src/com/java8/helloidea/collections/PropDemoDef.java
@@ -0,0 +1,41 @@
+package com.java8.helloidea.collections;
+
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * Use a default property list.
+ * Created by jianwei on 16/7/9.
+ */
+public class PropDemoDef {
+    public static void main(String args[]) {
+        Properties defList = new Properties();
+        defList.put("Florida", "Tallahassee");
+        defList.put("Wisconsin", "Madison");
+
+        Properties capitals = new Properties(defList);
+
+        capitals.put("Illinois", "Springfield");
+        capitals.put("Missouri", "Jefferson City");
+        capitals.put("Washington", "Olympia");
+        capitals.put("California", "Sacramento");
+        capitals.put("Indiana", "Indianapolis");
+
+        // Get a set-view of the keys.
+        Set<?> states = capitals.keySet();
+
+        // Show all of the states and capitals.
+        for(Object name : states)
+            System.out.println("The capital of " +
+                    name + " is " +
+                    capitals.getProperty((String)name)
+                    + ".");
+
+        System.out.println();
+
+        // Florida will now be found in the default list.
+        String str = capitals.getProperty("Florida");
+        System.out.println("The capital of Florida is "
+                + str + ".");
+    }
+}
